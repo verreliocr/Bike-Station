e4185ed09b03a80c679432e0610fa9c1bc40f99d
verrelio.cr <verrelioragna@gmail.com>
2022-10-23 19:57:40 +0700

    [Verrel] add station detail view

---
+++ Bike Station/Modules/Station Detail/StationDetailView.swift
@@ -0,0 +1,60 @@
+//
+//  StationDetailView.swift
+//  Bike Station
+//
+//  Created by Verrelio C. Rizky on 23/10/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct StationDetailView: View {
+    
+    let stationItem: stationItemModel
+    
+    @StateObject var locationManager = LocationManager()
+    
+    var stationCoordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: stationItem.geometry?.latitude ?? 0,
+                                      longitude: stationItem.geometry?.longitude ?? 0)
+    }
+    
+    var coordinateSpan: MKCoordinateSpan {
+        return MKCoordinateSpan(latitudeDelta: 0.01,
+                                longitudeDelta: 0.01)
+    }
+    
+    var region: MKCoordinateRegion {
+        return MKCoordinateRegion(center: stationCoordinate,
+                                  span: coordinateSpan)
+    }
+    
+    var annotations: [stationAnnotation] {
+        return [stationAnnotation(coordinate: stationCoordinate)]
+    }
+    
+    var body: some View {
+        VStack {
+            Map(coordinateRegion: .constant(region),
+                interactionModes: [],
+                showsUserLocation: true,
+                annotationItems: annotations,
+                annotationContent: { annotation in
+                MapAnnotation(coordinate: annotation.coordinate,
+                              content: {
+                    EmptyView()
+                })
+            })
+            .edgesIgnoringSafeArea(.top)
+            
+            StationItemRow(stationItem: stationItem)
+                .frame(height: 200)
+        }
+    }
+}
+
+struct StationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        StationDetailView(stationItem: stationItemModel.example)
+    }
+}
