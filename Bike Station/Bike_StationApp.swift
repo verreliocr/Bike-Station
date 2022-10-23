03c1f0b065faa315ae413718213fe2712c6fb5c2
verrelio.cr <verrelioragna@gmail.com>
2022-10-23 18:35:38 +0700

    Initial Commit

---
+++ Bike Station/Bike_StationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bike_StationApp.swift
+//  Bike Station
+//
+//  Created by Verrelio C. Rizky on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Bike_StationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
